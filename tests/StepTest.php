19400e9b881c13962fd288d074ae16f8124c76e1
deanar <rdeanar@gmail.com>
2017-04-26 19:36:06 +0300

    StepTest

---
+++ tests/StepTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Opti\tests;
+
+use Opti\Scenarios\Step;
+
+class StepTest extends TestCase
+{
+
+    protected function getStep()
+    {
+        $config = [
+            'bin'      => 'ls',
+            'template' => '{options}',
+            'configs'  => [
+                'default' => [
+                    '-lah',
+                ],
+            ],
+        ];
+
+        $logger = new \Psr\Log\NullLogger();
+        $tool = new \Opti\Tools\ConfigurableTool($logger);
+        $tool->configure($config);
+
+        $step = new Step($logger, $tool, 'default');
+
+        return $step;
+    }
+
+    public function testFromFile()
+    {
+        $step = $this->getStep();
+
+        $step->fromFile(__DIR__ . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'files' . DIRECTORY_SEPARATOR . 'Definition_of_Free_Cultural_Works_logo_notext.png');
+
+        $this->assertNotEmpty($step->getInputSize());
+    }
+}
