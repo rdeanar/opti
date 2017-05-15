9261dcb5f27fddf252267de129d3149f4e4569a3
deanar <rdeanar@gmail.com>
2017-05-16 00:29:33 +0300

    Added tools: guetzli, pngquant, optipng, svgo, svgcleaner.

--- run.sh
+++ run.sh
@@ -4,7 +4,15 @@
 #find ./data/ -type f | xargs php ./1.php
 
 cp data/fast/1.jpg data/fast/2.jpg
+cp data/fast/3.png data/fast/4.png
+cp data/fast/5.svg data/fast/6.svg
+
 du -hs data/fast/2.jpg
-#php ./1.php data/fast/2.jpg
-./bin/opti optimize -vvv ./data/fast/2.jpg
+du -hs data/fast/4.png
+du -hs data/fast/6.svg
+
+time ./bin/opti optimize -vvv ./data/fast/2.jpg ./data/fast/4.png ./data/fast/6.svg
+
 du -hs data/fast/2.jpg
+du -hs data/fast/4.png
+du -hs data/fast/6.svg
